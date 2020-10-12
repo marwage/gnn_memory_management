46319b56a645840fa05bcea7dbb305ded21d8bf9
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-10-12 17:46:41 +0200

    add test for loss forward

---
+++ tests/loss.cpp
@@ -0,0 +1,21 @@
+// Copyright 2020 Marcel Wagenländer
+
+#include <iostream>
+
+#include "tensors.hpp"
+#include "loss.hpp"
+
+
+void check_loss() {
+    std::string home = std::getenv("HOME");
+    std::string dir_path = home + "/gpu_memory_reduction/alzheimer/data/flickr";
+
+    std::string path = dir_path + "/log_softmax_out.npy";
+    matrix<float> log_softmax_out = load_npy_matrix<float>(path);
+    path = dir_path + "/classes.npy";
+    matrix<int> classes = load_npy_matrix<int>(path);
+
+    NLLLoss loss_layer;
+    float loss = loss_layer.forward(log_softmax_out, classes);
+    std::cout << "loss " << loss << std::endl;
+}
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,8 +2,10 @@
 
 void check_divmv();
 void check_log_softmax_forward();
+void check_loss();
 
 int main() {
     check_divmv();
     check_log_softmax_forward();
+    check_loss();
 }
