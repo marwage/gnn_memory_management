9da5f23b130b0e43fb87086ef931fcac0176d987
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-10-12 13:27:24 +0000

    add specific test for log_softmax.py

---
+++ check_log_softmax.py
@@ -0,0 +1,33 @@
+import json
+import numpy as np
+import scipy.sparse as sp
+import scipy.io
+import os
+import torch
+
+
+home = os.getenv("HOME")
+dir_path = home + "/gpu_memory_reduction/alzheimer/data/flickr"
+
+path = dir_path + "/features.npy"
+features = np.load(path)
+
+path = dir_path + "/log_softmax_out.npy"
+log_softmax_out = np.load(path)
+
+# to row-major
+n, m = features.shape
+features = features.reshape((m, n))
+features = features.transpose()
+log_softmax_out = log_softmax_out.reshape((m, n))
+log_softmax_out = log_softmax_out.transpose()
+
+log_softmax_layer = torch.nn.LogSoftmax(dim=-1)
+true_log_softmax_out = log_softmax_layer(torch.from_numpy(features))
+true_log_softmax_out = true_log_softmax_out.numpy()
+
+is_close = np.isclose(log_softmax_out, true_log_softmax_out)
+percentage_equal = is_close.sum() / true_log_softmax_out.size
+print("Log-softmax")
+print("Percentage of equal elements: {}".format(percentage_equal))
+
