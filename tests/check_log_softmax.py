368262480e5507c99aeb9517c238e6e6858d3c3e
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-10-13 07:23:49 +0000

    update python checks

--- tests/check_log_softmax.py
+++ tests/check_log_softmax.py
@@ -24,3 +24,4 @@ true_log_softmax_out = true_log_softmax_out.numpy()
 is_close = np.isclose(log_softmax_out, true_log_softmax_out)
 percentage_equal = is_close.sum() / true_log_softmax_out.size
 print("Log-softmax: Percentage of equal elements: {}".format(percentage_equal))
+
--- tests/check_loss.py
+++ tests/check_loss.py
@@ -18,7 +18,27 @@ log_softmax_out = log_softmax_out.reshape((m, n))
 log_softmax_out = log_softmax_out.transpose()
 
 loss_layer = torch.nn.NLLLoss()
-true_loss = loss_layer(torch.from_numpy(log_softmax_out), torch.from_numpy(classes))
-true_loss = true_loss.numpy()
+torch_log_softmax_out = torch.from_numpy(log_softmax_out)
+torch_log_softmax_out.requires_grad_()
+torch_classes = torch.from_numpy(classes).long()
+true_loss = loss_layer(torch_log_softmax_out, torch_classes)
+true_loss.backward()
+true_loss = true_loss.detach().numpy()
 
 print("Loss: {}".format(true_loss))
+
+path = dir_path + "/loss_grads.npy"
+loss_grads = np.load(path)
+
+# to row-major
+n, m = loss_grads.shape
+loss_grads = loss_grads.reshape((m, n))
+loss_grads = loss_grads.transpose()
+
+torch_loss_grads = torch_log_softmax_out.grad.numpy()
+
+assert(loss_grads.shape == torch_log_softmax_out.grad.shape)
+is_close = np.isclose(loss_grads, torch_loss_grads) 
+percentage_equal = is_close.sum() / loss_grads.size
+print("Loss backward: Percentage of equal elements: {}".format(percentage_equal))
+
