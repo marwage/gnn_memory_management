ebe8d69eed5282a0214adb5da05604345c032f56
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-11-11 13:52:39 +0000

    add and update tests (python)

--- tests/adam.py
+++ tests/adam.py
@@ -38,39 +38,68 @@ def num_close_rows(A, B):
     
     return close_rows.sum()
 
+def print_small(A):
+    print(A[0:3, 0:3])
+
+def to_torch(a):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    a_torch = torch.from_numpy(a)
+    a_torch = a_torch.to(device)
+    a_torch.requires_grad_()
+    a_torch.retain_grad()
+
+    return a_torch
+
 
 def test_adam():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     home = os.getenv("HOME")
     dir_path = home + "/gpu_memory_reduction/alzheimer/data"
     flickr_dir_path = dir_path + "/flickr"
     test_dir_path = dir_path + "/tests"
 
-    for i in range(4):
-        path = test_dir_path + "/gradient_" + str(i) + ".npy"
-        gradient = load_col_major(path)
+    path = test_dir_path + "/weight.npy"
+    weight = load_col_major(path)
+    path = test_dir_path + "/bias.npy"
+    bias = load_col_major(path)
+    path = test_dir_path + "/weight_grads.npy"
+    weight_grads = load_col_major(path)
+    path = test_dir_path + "/bias_grads.npy"
+    bias_grads = load_col_major(path)
 
-        path = test_dir_path + "/gradient_chunked_" + str(i) + ".npy"
-        gradient_chunked = load_col_major(path)
+    weight_torch = to_torch(weight)
+    bias_torch = to_torch(bias)
+    weight_grads_torch = torch.from_numpy(weight_grads)
+    weight_grads_torch = weight_grads_torch.to(device)
+    bias_grads_torch = torch.from_numpy(bias_grads)
+    bias_grads_torch = bias_grads_torch.to(device)
 
-        ratio = check_isclose(gradient, gradient_chunked)
-        print("Gradient of layer {}: Ratio {}".format(i, ratio))
+    learning_rate = 0.003
+    params = [weight_torch, bias_torch]
+    optimiser = torch.optim.Adam(params, lr=learning_rate)
+    optimiser.zero_grad()
 
-        path = test_dir_path + "/weight_" + str(i) + ".npy"
-        weight = load_col_major(path)
+    weight_torch.grad = weight_grads_torch
+    bias_torch.grad = bias_grads_torch
 
-        path = test_dir_path + "/weight_chunked_" + str(i) + ".npy"
-        weight_chunked = load_col_major(path)
+    optimiser.step()
 
-        ratio = check_isclose(weight, weight_chunked)
-        print("Weight of layer {}: Ratio {}".format(i, ratio))
+    path = test_dir_path + "/weight_updated.npy";
+    weight_updated = load_col_major(path)
+    path = test_dir_path + "/bias_updated.npy";
+    bias_updated = load_col_major(path)
 
-        # debug
-        print("gradients")
-        print(gradient[0:5, 0:5])
-        print(gradient_chunked[0:5, 0:5])
-        print("weights")
-        print(weight[0:5, 0:5])
-        print(weight_chunked[0:5, 0:5])
+    true_weight = weight_torch.detach().cpu().numpy()
+    ratio_equal = check_isclose(weight_updated, true_weight)
+    print("Adam weight: Ratio of equal elements {}".format(ratio_equal))
+
+    true_bias = bias_torch.detach().cpu().numpy()
+    ratio_equal = check_isclose(bias_updated, true_bias)
+    print("Adam bias: Ratio of equal elements {}".format(ratio_equal))
+    #  print_small(bias_updated)
+    #  print_small(true_bias)
 
 
 if __name__ == "__main__":
--- tests/sage_linear_adam.py
+++ tests/sage_linear_adam.py
@@ -13,7 +13,6 @@ def load_col_major(path):
 
     return mat
 
-
 def check_isclose(A, B):
     if (A.shape == B.shape):
         is_close = np.isclose(A, B)
@@ -39,8 +38,11 @@ def num_close_rows(A, B):
     
     return close_rows.sum()
 
+def print_small(A):
+    print(A[0:3, 0:3])
 
-def test_sage_linear():
+
+def test_sage_linear_adam():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     home = os.getenv("HOME")
@@ -48,6 +50,7 @@ def test_sage_linear():
     flickr_dir_path = dir_path + "/flickr"
     test_dir_path = dir_path + "/tests"
 
+    # FORWARD
     path = test_dir_path + "/input_self.npy"
     input_self = load_col_major(path)
     path = test_dir_path + "/input_neigh.npy"
@@ -63,8 +66,6 @@ def test_sage_linear():
     input_neigh_torch = input_neigh_torch.to(device)
     input_neigh_torch.requires_grad_()
     input_neigh_torch.retain_grad()
-    in_gradients_torch = torch.from_numpy(in_gradients)
-    in_gradients_torch = in_gradients_torch.to(device)
 
     path = test_dir_path + "/self_weight.npy"
     self_weight = load_col_major(path)
@@ -74,8 +75,8 @@ def test_sage_linear():
     neigh_weight = load_col_major(path)
     path = test_dir_path + "/neigh_bias.npy"
     neigh_bias = load_col_major(path)
-    path = test_dir_path + "/result.npy"
-    sage_linear_result = load_col_major(path)
+    path = test_dir_path + "/activations.npy"
+    activations = load_col_major(path)
 
     self_weight_torch = torch.from_numpy(self_weight)
     self_weight_torch = self_weight_torch.to(device)
@@ -85,7 +86,6 @@ def test_sage_linear():
     self_bias_torch = self_bias_torch.to(device)
     self_bias_torch.requires_grad_()
     self_bias_torch.retain_grad()
-
     neigh_weight_torch = torch.from_numpy(neigh_weight)
     neigh_weight_torch = neigh_weight_torch.to(device)
     neigh_weight_torch.requires_grad_()
@@ -95,16 +95,25 @@ def test_sage_linear():
     neigh_bias_torch.requires_grad_()
     neigh_bias_torch.retain_grad()
 
+    learning_rate = 0.003
+    params = [self_weight_torch,
+        self_bias_torch,
+        neigh_weight_torch,
+        neigh_bias_torch]
+    optimiser = torch.optim.Adam(params, lr=learning_rate)
+    optimiser.zero_grad()
+
     self_result_torch = torch.matmul(input_self_torch, self_weight_torch) + self_bias_torch.T
     neigh_result_torch = torch.matmul(input_neigh_torch, neigh_weight_torch) + neigh_bias_torch.T
     true_sage_linear_result_torch = self_result_torch + neigh_result_torch
 
     true_sage_linear_result = true_sage_linear_result_torch.detach().cpu().numpy()
-
-    percentage_equal = check_isclose(sage_linear_result, true_sage_linear_result)
+    percentage_equal = check_isclose(activations, true_sage_linear_result)
     print("SageLinear: Percentage of equal elements: {}".format(percentage_equal))
 
     # BACKPROPAGATION
+    in_gradients_torch = torch.from_numpy(in_gradients)
+    in_gradients_torch = in_gradients_torch.to(device)
     true_sage_linear_result_torch.backward(in_gradients_torch)
 
     path = test_dir_path + "/self_grads.npy"
@@ -136,12 +145,72 @@ def test_sage_linear():
     print("Linear self weight: Ratio {}".format(ratio_equal))
     ratio_equal = check_isclose(self_bias_grads, true_self_bias_grads)
     print("Linear self bias: Ratio {}".format(ratio_equal))
+    #  print_small(self_bias_grads)
+    #  print_small(true_self_bias_grads)
     ratio_equal = check_isclose(neigh_weight_grads, true_neigh_weight_grads)
     print("Linear neigh weight: Ratio {}".format(ratio_equal))
     ratio_equal = check_isclose(neigh_bias_grads, true_neigh_bias_grads)
     print("Linear neigh bias: Ratio {}".format(ratio_equal))
+    #  print_small(neigh_bias_grads)
+    #  print_small(true_neigh_bias_grads)
+
+    # ADAM
+    optimiser.step()
+
+    path = test_dir_path + "/self_weight_updated.npy";
+    self_weight_updated = load_col_major(path)
+    path = test_dir_path + "/self_bias_updated.npy";
+    self_bias_updated = load_col_major(path)
+    path = test_dir_path + "/neigh_weight_updated.npy";
+    neigh_weight_updated = load_col_major(path)
+    path = test_dir_path + "/neigh_bias_updated.npy";
+    neigh_bias_updated = load_col_major(path)
+
+    true_self_weight = self_weight_torch.detach().cpu().numpy()
+    ratio_equal = check_isclose(self_weight_updated, true_self_weight)
+    print("Adam self weight: Ratio of equal elements {}".format(ratio_equal))
+    #  print_small(weight_self)
+    #  print_small(true_self_weight)
+    true_self_bias = self_bias_torch.detach().cpu().numpy()
+    ratio_equal = check_isclose(self_bias_updated, true_self_bias)
+    print("Adam self bias: Ratio of equal elements {}".format(ratio_equal))
+    #  print_small(weight_self_bias)
+    #  print_small(true_self_bias)
+    ratio_equal = check_isclose(neigh_weight_updated, neigh_weight_torch.detach().cpu().numpy())
+    print("Adam neigh weight: Ratio of equal elements {}".format(ratio_equal))
+    true_neigh_bias = neigh_bias_torch.detach().cpu().numpy()
+    ratio_equal = check_isclose(neigh_bias_updated, true_neigh_bias) 
+    print("Adam neigh bias: Ratio of equal elements {}".format(ratio_equal))
+    #  print_small(weight_neigh_bias)
+    #  print_small(true_neigh_bias)
+
+
+def compare_adam():
+    home = os.getenv("HOME")
+    dir_path = home + "/gpu_memory_reduction/alzheimer/data"
+    flickr_dir_path = dir_path + "/flickr"
+    test_dir_path = dir_path + "/tests"
+
+    for i in range(4):
+        path = test_dir_path + "/gradient_" + str(i) + ".npy"
+        gradient = load_col_major(path)
+
+        path = test_dir_path + "/gradient_chunked_" + str(i) + ".npy"
+        gradient_chunked = load_col_major(path)
+
+        ratio = check_isclose(gradient, gradient_chunked)
+        print("Gradient of layer {}: Ratio {}".format(i, ratio))
+
+        path = test_dir_path + "/weight_" + str(i) + ".npy"
+        weight = load_col_major(path)
+
+        path = test_dir_path + "/weight_chunked_" + str(i) + ".npy"
+        weight_chunked = load_col_major(path)
+
+        ratio = check_isclose(weight, weight_chunked)
+        print("Weight of layer {}: Ratio {}".format(i, ratio))
 
 
 if __name__ == "__main__":
-    test_sage_linear()
+    test_sage_linear_adam()
 
