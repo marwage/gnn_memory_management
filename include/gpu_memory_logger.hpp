94dce8ef7a5465a806c4b2a0aa35ee5df36a591b
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-12-09 16:12:09 +0100

    add parameter interval to gpu memory logger and opening and closing of file to start and stop

--- include/gpu_memory_logger.hpp
+++ include/gpu_memory_logger.hpp
@@ -6,16 +6,20 @@
 #include <future>
 #include <string>
 #include <thread>
+#include <fstream>
 
 
 class GPUMemoryLogger {
 private:
     std::string file_name_;
     std::string path_;
+    long interval_;
     std::promise<void> signal_exit_;
     std::thread logging_thread_;
+    std::ofstream log_file_;
 
 public:
+    GPUMemoryLogger(std::string file_name, long interval);
     GPUMemoryLogger(std::string file_name);
     void start();
     void stop();
