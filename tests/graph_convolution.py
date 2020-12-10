9387b2e95655871359fc89d956df6589a5023e25
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-12-10 11:46:13 +0000

    add option to feature aggregation for mean

--- tests/graph_convolution.py
+++ tests/graph_convolution.py
@@ -4,10 +4,11 @@ import scipy.io
 import scipy.sparse as sp
 import torch
 from helper import (check_close_equal, print_close_equal, write_equal,
-                    to_torch, print_small, count_nans)
+                    to_torch, count_nans)
 
 
 def check_graph_conv():
+    do_mean = True
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     all_close = 1.0
     home = os.getenv("HOME")
@@ -24,13 +25,13 @@ def check_graph_conv():
     graph_conv_result = np.load(path)
 
     num_nans = count_nans(graph_conv_result)
-    if (num_nans > 0):
+    if num_nans > 0:
         print("Graph Convolution: Number of NaNs: {}".format(num_nans))
         all_close = 0.0
 
     features_torch = to_torch(features)
 
-    assert (sp.isspmatrix_coo(adj))
+    assert sp.isspmatrix_coo(adj)
     values = adj.data
     indices = np.vstack((adj.row, adj.col))
     indices = torch.LongTensor(indices)
@@ -41,10 +42,11 @@ def check_graph_conv():
 
     graph_conv_result_torch = torch.sparse.mm(adj_torch, features_torch)
 
-    sum_torch = torch.sparse.sum(adj_torch, dim=-1)
-    sum_torch = sum_torch.to_dense()
-    for i in range(graph_conv_result_torch.shape[1]):
-        graph_conv_result_torch[:, i] = graph_conv_result_torch[:, i] / sum_torch
+    if do_mean:
+        sum_torch = torch.sparse.sum(adj_torch, dim=-1)
+        sum_torch = sum_torch.to_dense()
+        for i in range(graph_conv_result_torch.shape[1]):
+            graph_conv_result_torch[:, i] = graph_conv_result_torch[:, i] / sum_torch
 
     true_graph_conv_result = graph_conv_result_torch.detach().cpu().numpy()
     ratio_close, ratio_equal = check_close_equal(graph_conv_result, true_graph_conv_result)
